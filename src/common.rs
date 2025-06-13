5e65b17c9242dca2956b4fd939213caae0558d3c
sxdpf <97816286+sxdpf@users.noreply.github.com>
2025-06-13 22:18:00 +0800

    Update common.rs

--- src/common.rs
+++ src/common.rs
@@ -1003,7 +1003,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://www.sxsckj.dpdns.org:21114".to_owned()
 }
 
 #[inline]
